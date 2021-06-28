4f230874487636fc8db21abaf26f9c5699f7d723
tinkyrain <65303982+tinkyrain@users.noreply.github.com>
2021-06-28 13:43:28 +0500

    v.0.1
    Added Placeholder in Main. The user can do blocks with a task, removed Footer

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
+//Obtain all the necessary blocks and input fields
 let input = document.getElementById('input');
 let main = document.getElementById('main');
+let main_placeholder = document.getElementById('main-placeholder');
 
+//When updating the page, the pure value
+input.value = '';
+
+//massive tasks
+let tasks_arr = [];
+
+//Function for creating a block with a task
 function createTaskBlock(){
   let task_block = document.createElement('div');
   main.appendChild(task_block);
   task_block.className = 'task_block';
 
   task_block.innerText = `${input.value}`;
-
-  console.log('-');
 }
 
+function tasks(){
+  tasks_arr.push(input.value);
+  //clear field
+  input.value = '';
+
+  let numTasks = tasks_arr.length;
+
+  if(numTasks > 0){
+    main_placeholder.style.display = 'none';
+  } else {
+    main_placeholder.style.display = 'block';
+  }
+}
+
+//Check Field value
 function CheckFieldFill(){
   if(input.value == ''){
     input.style.border = '1px solid red';
   } else {
     createTaskBlock();
+    tasks();
   }
 }
 
+//Creating an ENTER Press Block
 document.addEventListener('keydown', function(event){
   if(event.which === 13){
     CheckFieldFill();
-
-    console.log('*');
   }
 });
